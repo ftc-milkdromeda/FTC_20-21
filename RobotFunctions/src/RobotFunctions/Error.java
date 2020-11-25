ee6c0eeae5f4293c9cecbd470af1a2ab4c190c2f
Tyler Wang <wangtyler123@gmail.com>
2020-11-25 16:40:10 -0600

    Fixed merge error.

--- RobotFunctions/src/RobotFunctions/Error.java
+++ RobotFunctions/src/RobotFunctions/Error.java
@@ -1,11 +1,6 @@
 package RobotFunctions;
 
 public enum Error {
-    CLASS_NOT_ACTIVE(-1),
-    DIVISION_BY_ZERO(-2),
-    ARGUMENTS_OUT_OF_BOUND(-3),
-    INVALID_STATES(-4),
-
     //errors for MecanumWheels.MecanumWheels
     PROCESS_ALREADY_RUNNING(001),
     NO_PROCESS_RUNNING(002),
@@ -19,7 +14,12 @@ public enum Error {
     //errors for GamePad.KeyMappings
     KM_PREVIOUS_THREAD_ALREADY_STARTED(201),
 
+    CLASS_NOT_ACTIVE(-1),
+    DIVISION_BY_ZERO(-2),
+    ARGUMENTS_OUT_OF_BOUND(-3),
+    INVALID_STATES(-4),
     NO_ERROR(0);
+
     private int errorCode;
     private Error(int errorCode) {
         this.errorCode = errorCode;

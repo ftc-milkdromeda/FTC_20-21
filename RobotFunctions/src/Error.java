b29a78dedc1e85c526c172a87270037dfe93795a
Tyler Wang <wangtyler123@gmail.com>
2020-11-25 15:43:13 -0600

    Changed error file.

--- RobotFunctions/src/Error.java
+++ RobotFunctions/src/Error.java
@@ -1,5 +1,13 @@
 public enum Error {
-    CLASS_NOT_ACTIVE(-1), NO_ERROR(0), DIVISION_BY_ZERO(1), ARGUMENTS_OUT_OF_BOUND(2), INVALID_STATES(3);
+    CLASS_NOT_ACTIVE(-1),
+    NO_ERROR(0),
+    DIVISION_BY_ZERO(-2),
+    ARGUMENTS_OUT_OF_BOUND(-3),
+    INVALID_STATES(-4),
+
+    //errors for mecanum wheels
+    INCORRECT_PROCEDURE_MODE(001),
+    PROCEDURE_MODE_NOT_INIT(001);
 
     public int errorCode;
     private Error(int errorCode) {

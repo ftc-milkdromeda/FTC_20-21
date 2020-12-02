87198c4860b1a6806226143beaab0056e0ee4017
Tyler Wang <wangtyler123@gmail.com>
2020-12-02 00:45:10 -0600

    Fixed rebase error.

---
+++ RobotFunctions/src/RobotFunctions/MecanumWheels/Procedure.java
@@ -0,0 +1,19 @@
+package RobotFunctions.MecanumWheels;
+
+public class Procedure{
+    public Procedure(double angle, double magnitude, double pivot) {}
+
+    public double getAngle() {
+        return this.angle;
+    }
+    public double getMagnitude() {
+        return this.magnitude;
+    }
+    public double getPivot() {
+        return this.pivot;
+    }
+
+    private double angle;
+    private double magnitude;
+    private double pivot;
+}

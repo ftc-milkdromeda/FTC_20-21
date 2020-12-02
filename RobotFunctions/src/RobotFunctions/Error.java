59a944aafcc1d597a1b7539b4c14a8724ad35164
Tyler Wang <wangtyler123@gmail.com>
2020-12-02 01:05:36 -0600

    Added errors codes.

--- RobotFunctions/src/RobotFunctions/Error.java
+++ RobotFunctions/src/RobotFunctions/Error.java
@@ -2,15 +2,21 @@ package RobotFunctions;
 
 public enum Error {
     CLASS_NOT_ACTIVE(-1),
-    NO_ERROR(0),
     DIVISION_BY_ZERO(-2),
     ARGUMENTS_OUT_OF_BOUND(-3),
     INVALID_STATES(-4),
 
-    //errors for mecanum wheels
-    INCORRECT_PROCEDURE_MODE(001),
-    PROCEDURE_MODE_NOT_INIT(001);
+    //errors for MecanumWheels.MecanumWheels
+    PROCESS_ALREADY_RUNNING(001),
+    NO_PROCESS_RUNNING(002),
+    NO_PROCEDURE_SET(003),
 
+    //errors for MecanumWheels.Operation
+    EXECUTION_STOPPED_FORCEFULLY(101),
+    NO_TEMPLATE_PROVIDED(102),
+    THREAD_NOT_STARTED(103),
+
+    NO_ERROR(0);
     private int errorCode;
     private Error(int errorCode) {
         this.errorCode = errorCode;

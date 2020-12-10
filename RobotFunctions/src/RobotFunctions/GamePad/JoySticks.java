0063aff960a1f104627aed56724f35800d6690c5
Tyler Wang <wangtyler123@gmail.com>
2020-12-10 00:17:15 -0600

    Added class to deal with joystick inputs; still need to work on polar conversions.

---
+++ RobotFunctions/src/RobotFunctions/GamePad/JoySticks.java
@@ -0,0 +1,14 @@
+package RobotFunctions.GamePad;
+
+public class JoySticks {
+    public double X;
+    public double Y;
+
+    public double getMagnitude() {
+
+    }
+    public double getAngle() {
+
+    }
+}
+//todo fill in polar conversions.

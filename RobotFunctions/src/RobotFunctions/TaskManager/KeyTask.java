5ea4804d62e57831f902f43b7bd9cd856fd16d30
Tyler Wang <wangtyler123@gmail.com>
2021-01-06 12:51:36 -0600

    Added class to make task that require key activation.

--- RobotFunctions/src/RobotFunctions/TaskManager/KeyTask.java
+++ RobotFunctions/src/RobotFunctions/TaskManager/KeyTask.java
@@ -8,6 +8,38 @@ public abstract class KeyTask extends Task{
 
     protected abstract double[] keyMapping();
 
+    public abstract static class Toggle extends Thread{
+        public Toggle(Controller controller, Clock clock) {
+            this.controller = controller;
+            this.state = false;
+            this.clock = clock;
+        }
+
+        protected abstract boolean testKey();
+
+        @Override
+        public void run() {
+            while(!super.isInterrupted()) {
+                while (!super.isInterrupted() && !this.testKey());
+
+                this.state = this.state ? false : true;
+
+                while (!super.isInterrupted() && this.testKey());
+            }
+        }
+        public void terminate() {
+            this.interrupt();
+        }
+
+        public boolean getToggleState() {
+            return this.state;
+        }
+
+        private Clock clock;
+        private boolean state;
+        protected Controller controller;
+    }
+
     protected Controller controller;
 }
 

1fdaefda21f1ba2a6a2522b0e907e6114131d735
Tyler Wang <wangtyler123@gmail.com>
2021-01-10 21:19:56 -0600

    Changed driver class so that only one thread can use at a time.

--- RobotFunctions/src/Drivers/Template/Driver.java
+++ RobotFunctions/src/Drivers/Template/Driver.java
@@ -1,8 +1,73 @@
 package Drivers.Template;
 
+import Drivers.DriverManager;
+import TaskManager.Task;
+
 public abstract class Driver {
-    public abstract void terminate();
+    protected Driver() {
+        this.interrupted = false;
+        this.processId = -1;
+        this.interruptId = -1;
 
-    protected boolean status;
+        this.alive = true;
+        this.busy = false;
+    }
 
+    public final boolean enterThread(Task task) {
+        if(this.processId != -1)
+            return false;
+
+        this.processId = task.getProcessId();
+        return true;
+    }
+    public final boolean exitThread(Task task) {
+        if(task.getProcessId() == this.processId) {
+            this.processId = -1;
+            return true;
+        }
+
+        return false;
+    }
+    public final boolean interject(Task task) {
+        if(this.interruptId != -1)
+            return false;
+
+        this.interruptId = task.getProcessId();
+        this.interrupted = true;
+        return true;
+    }
+    public final boolean returnMainThread(Task task) {
+        if(task.getProcessId() == this.processId) {
+            this.interruptId = -1;
+            this.interrupted = false;
+            return true;
+        }
+
+        return false;
+    }
+
+    public final void terminate() {
+        this.destructor();
+        this.alive = false;
+    }
+    protected void destructor() {}
+
+    public boolean isAlive() {
+        return this.alive;
+    }
+
+    protected final boolean testTask(Task task) {
+        return (!this.interrupted && this.getProcessId() == this.processId || this.interrupted && task.getProcessId() == this.interruptId);
+    }
+
+    public int getProcessId() {
+        return this.processId;
+    }
+
+    private int processId;
+    private int interruptId;
+
+    protected boolean busy;
+    protected boolean interrupted;
+    protected boolean alive;
 }

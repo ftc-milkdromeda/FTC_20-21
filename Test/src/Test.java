e718dfd12397ae7f67feb10c64a544a2a3d78f3d
Tyler Wang <wangtyler123@gmail.com>
2021-01-17 00:11:17 -0600

    Added new image library.

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,28 +1,6 @@
-import RobotFunctions.MecanumWheels.Drive;
-import RobotFunctions.MecanumWheels.Moter;
-import RobotFunctions.MecanumWheels.Procedure;
-import RobotFunctions.MecanumWheels.RoughMecanumWheels;
-import RobotFunctions.Units;
-
 public class Test {
     public static void main(String [] args) {
-        Drive print = new Print();
-        RoughMecanumWheels wheels = RoughMecanumWheels.instance(print, 18, 18, Units.IN);
 
-        Procedure operation = new Procedure(Math.PI * .25, 1.0, 0.5);
-        wheels.addTrojectory(operation);
-        wheels.drive();
     }
 }
 
-class Print extends Drive {
-    @Override
-    public void setMoter(Moter index, double power) {
-        System.out.println(index.getValue() + ": " + (float)power);
-    }
-    @Override
-    public void setMoters(double[] powers) {
-        for(int a = 0; a < 4; a++)
-            System.out.println(a + ": " + (float)powers[a]);
-    }
-}

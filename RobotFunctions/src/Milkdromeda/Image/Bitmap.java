43237ebaf534ef62382c36a34b5d63b4bda15d0e
Tyler Wang <wangtyler123@gmail.com>
2021-01-18 16:52:02 -0600

    Added and tested image adjustment code.

--- RobotFunctions/src/Milkdromeda/Image/Bitmap.java
+++ RobotFunctions/src/Milkdromeda/Image/Bitmap.java
@@ -1,7 +1,6 @@
 package Milkdromeda.Image;
 
 import io.nayuki.bmpio.*;
-import io.nayuki.bmpio.Rgb888Image;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -11,6 +10,22 @@ public class Bitmap {
     public Bitmap(int width, int height) {
         this.width = width;
         this.height = height;
+
+        this.pixels = null;
+    }
+    public Bitmap(Bitmap obj) {
+        this.width = obj.width;
+        this.height = obj.height;
+        this.grayscale = obj.grayscale;
+
+        if(obj.pixels != null) {
+            this.pixels = new double[obj.pixels.length];
+
+            for (int a = 0; a < this.pixels.length; a++)
+                this.pixels[a] = obj.pixels[a];
+        }
+        else
+            obj.pixels = null;
     }
 
     public boolean setPixels(double[] pixels, boolean grayscale) {
@@ -35,15 +50,30 @@ public class Bitmap {
 
         this.pixels = new double[pixels.length];
         for(int a = 0; a < pixels.length; a++)
-            this.pixels[a] = pixels[a] / 256f;
+            this.pixels[a] = pixels[a] / 255f;
 
         this.grayscale = grayscale;
 
         return true;
     }
+    public boolean setPixels(byte[] pixels, boolean grayscale) {
+        int buffer[] = new int[pixels.length];
+
+        for(int a = 0; a < buffer.length; a++)
+            buffer[a] = 0xff & pixels[a];
+
+        return this.setPixels(buffer, grayscale);
+    }
 
     public double[] getPixels() {
         return this.pixels;
+    }
+    public double[] getPixel(int x, int y) {
+        return new double[] {
+          this.pixels[(x * this.height + y) * (this.grayscale ? 1 : 3) + (this.grayscale ? 0 : 0)],
+          this.pixels[(x * this.height + y) * (this.grayscale ? 1 : 3) + (this.grayscale ? 0 : 1)],
+          this.pixels[(x * this.height + y) * (this.grayscale ? 1 : 3) + (this.grayscale ? 0 : 2)]
+        };
     }
     public boolean isGrayscale() {
         return this.grayscale;
@@ -56,17 +86,19 @@ public class Bitmap {
     }
 
     public void makeGrayscale() {
-        if(this.grayscale)
+        if(this.grayscale || this.pixels == null)
             return;
 
         double pixels[] = new double[this.width * this.height];
-        for(int a = 0; a < pixels.length; a+=3)
-            pixels[a] = (this.pixels[a] + this.pixels[a + 1] + this.pixels[a + 2]) / 3;
+        for(int a = 0; a < this.pixels.length; a+=3)
+            pixels[a / 3] = (this.pixels[a] + this.pixels[a + 1] + this.pixels[a + 2]) / 3;
 
         this.grayscale = true;
         this.pixels = pixels;
     }
     public boolean crop(int x1, int y1, int x2, int y2) {
+        if(this.pixels == null)
+            return false;
        if(x1 > x2 || y1 > y2)
            return false;
        if(x2 >= this.width || y2 >= this.height)
@@ -74,30 +106,53 @@ public class Bitmap {
 
        double pixels[] = new double[(grayscale ? 1 : 3) *(x2 - x1) * (y2 - y1)];
 
-       for(int x = x1, counter = 0; x < x2; x++) {
-           for(int y = y1; y < y2; y++, counter++) {
-               pixels[counter] = this.pixels[x * this.height + y];
+       for(int x = x1, x_counter = 0; x < x2; x++, x_counter++) {
+           for(int y = y1, y_counter = 0; y < y2; y++, y_counter++) {
+               for(int a = 0; a < (this.grayscale ? 1 : 3); a++) {
+                    int multiple = this.grayscale ? 1 : 3;
+                   pixels[(x_counter * (y2 - y1) + y_counter) * multiple + a] = this.pixels[(x * this.height + y) * multiple + a];
+               }
            }
        }
+
+        this.width = x2 - x1;
+        this.height = y2 - y1;
 
        this.pixels =  pixels;
 
        return true;
     }
+    public boolean rotate() {
+
+        return true;
+    }
+    public boolean rotateCounter() {
+        double[] rotated = new double[this.pixels.length];
+
+        for(int x = 0; x < this.height; x++) {
+            for(int y = 0; y < this.width; y++) {
+                //rotated[x * this.width + y] = this.pixels[]
+            }
+        }
+        return true;
+    }
 
     public boolean writeImage(String name) {
+        if(this.pixels == null)
+            return false;
+
         BmpImage bmp = new BmpImage();
         bmp.image = new AbstractRgb888Image(this.width, this.height) {
             @Override
             public int getRgb888Pixel(int x, int y) {
                 if(Bitmap.this.grayscale) {
-                    byte luminance = (byte) (Bitmap.this.pixels[x * this.height + y] * 256);
+                    int luminance = (int) (Bitmap.this.pixels[x * this.height + y] * 255);
                     return luminance << 0 | luminance << 8 | luminance << 16;
                 }
                 else {
-                    int r = (int) (Bitmap.this.pixels[(x * this.height + y) * 3 + 0] * 256);
-                    int g = (int) (Bitmap.this.pixels[(x * this.height + y) * 3 + 1] * 256);
-                    int b = (int) (Bitmap.this.pixels[(x * this.height + y) * 3 + 2] * 256);
+                    int r = (int) (Bitmap.this.pixels[(x * this.height + y) * 3 + 0] * 255);
+                    int g = (int) (Bitmap.this.pixels[(x * this.height + y) * 3 + 1] * 255);
+                    int b = (int) (Bitmap.this.pixels[(x * this.height + y) * 3 + 2] * 255);
                     return b << 0| g << 8 | r << 16;
                 }
 
@@ -131,9 +186,9 @@ public class Bitmap {
 
         for(int x = 0; x < image.image.getWidth(); x++) {
             for(int y = 0; y < image.image.getHeight(); y++) {
-                this.pixels[(x * image.image.getHeight() + y) * 3 + 0] = ((0x00ff0000 & image.image.getRgb888Pixel(x, y)) >> 16)  / 256f;
-                this.pixels[(x * image.image.getHeight() + y) * 3 + 1] = ((0x0000ff00 & image.image.getRgb888Pixel(x, y)) >>  8) / 256f;
-                this.pixels[(x * image.image.getHeight() + y) * 3 + 2] = ((0x000000ff & image.image.getRgb888Pixel(x, y)) >>  0) / 256f;
+                this.pixels[(x * image.image.getHeight() + y) * 3 + 0] = ((0x00ff0000 & image.image.getRgb888Pixel(x, y)) >> 16)  / 255f;
+                this.pixels[(x * image.image.getHeight() + y) * 3 + 1] = ((0x0000ff00 & image.image.getRgb888Pixel(x, y)) >>  8) / 255f;
+                this.pixels[(x * image.image.getHeight() + y) * 3 + 2] = ((0x000000ff & image.image.getRgb888Pixel(x, y)) >>  0) / 255f;
             }
         }
 
@@ -142,6 +197,36 @@ public class Bitmap {
         this.width = image.image.getWidth();
 
         return false;
+    }
+
+    public boolean adjustCurve(Curve curve) {
+        if(this.pixels == null)
+            return false;
+
+        for(int a = 0; a < this.pixels.length; a++)
+            this.pixels[a] = curve.curve(this.pixels[a]);
+
+        return true;
+    }
+    public static abstract class Curve {
+        public abstract double curve(double input);
+    }
+
+    public boolean adjustLuminance(double blackLevel, double whiteLevel) {
+        if(this.pixels == null)
+            return false;
+
+        double range = whiteLevel - blackLevel;
+        for(int a = 0; a < this.pixels.length; a++) {
+            this.pixels[a] = (this.pixels[a] - blackLevel) / range;
+
+            if(this.pixels[a] < 0)
+                this.pixels[a] = 0.0;
+            else if(this.pixels[a] > whiteLevel)
+                this.pixels[a] = 1.0;
+        }
+
+        return true;
     }
 
     private int width;
--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,23 +1,111 @@
 import Milkdromeda.Image.Bitmap;
 
-public class Test {
-    public static void main(String [] args) {
-        Bitmap bitmap = new Bitmap(500, 500);
+import java.nio.ByteBuffer;
 
-        bitmap.readImage("bitmap");
+public class Test {
+    private static int countPixel(Bitmap image, double lowerBound, double upperBound) {
+        image.makeGrayscale();
+
+        double pixels[] = image.getPixels();
+        int numOfPixel = 0;
+
+        if(pixels == null)
+            return 0;
+
+        for(int a = 0; a < pixels.length; a++) {
+            numOfPixel += (pixels[a] > lowerBound && pixels[a] < upperBound) ? 1 : 0;
+        }
+
+        return numOfPixel;
+    }
+
+    public static void main(String [] args) {
+        Bitmap bitmap0 = new Bitmap(500, 500);
+        Bitmap bitmap1 = new Bitmap(500, 500);
+        Bitmap bitmap2 = new Bitmap(500, 500);
+
+        bitmap0.readImage("image");
+        bitmap1.readImage("bitmap1");
+        bitmap2.readImage("bitmap2");
+
+        bitmap0.makeGrayscale();
+        bitmap1.makeGrayscale();
+        bitmap2.makeGrayscale();
+
+        final int sampleSize = 100;
+        final int bl_x = 540;
+        final int wl_x = 100;
+
+        final int bl_y = 590;
+        final int wl_y = 730;
+
+        double bl_0 = 0;
+        double bl_1 = 0;
+        double bl_2 = 0;
+
+        double wl_0 = 0;
+        double wl_1 = 0;
+        double wl_2 = 0;
+
+        for(int a = 0; a < sampleSize; a++) {
+            bl_0 += bitmap0.getPixel(bl_x, bl_y + a)[0];
+            bl_1 += bitmap1.getPixel(bl_x, bl_y + a)[0];
+            bl_2 += bitmap2.getPixel(bl_x, bl_y + a)[0];
+
+            wl_0 += bitmap0.getPixel(wl_x + a, wl_y)[0];
+            wl_1 += bitmap1.getPixel(wl_x + a, wl_y)[0];
+            wl_2 += bitmap2.getPixel(wl_x + a, wl_y)[0];
+        }
+
+        bl_0 /= sampleSize;
+        bl_1 /= sampleSize;
+        bl_2 /= sampleSize;
+
+        wl_0 /= sampleSize;
+        wl_1 /= sampleSize;
+        wl_2 /= sampleSize;
+
+        bitmap0.crop(84, 436, 234, 696);
+        bitmap1.crop(84, 436, 234, 696);
+        bitmap2.crop(84, 436, 234, 696);
+
+        bitmap0.adjustLuminance(bl_0, wl_0);
+        bitmap1.adjustLuminance(bl_1, wl_1);
+        bitmap2.adjustLuminance(bl_2, wl_2);
+
 /*
-        int buffer[] = new int[500 * 500 * 3];
+        byte buffer[] = new byte[500 * 500 * 3];
         for(int x = 0; x < 500; x++) {
             for(int y = 0; y < 500; y++) {
-                buffer[(x * 500 + y) * 3 + 0] = 0;
-                buffer[(x * 500 + y) * 3 + 1] = 0;
-                buffer[(x * 500 + y) * 3 + 2] = 255;
+                buffer[(x * 500 + y) * 3 + 0] = 127;
+                buffer[(x * 500 + y) * 3 + 1] = -100;
+                buffer[(x * 500 + y) * 3 + 2] = -1;
             }
         }
 
-        bitmap.setPixels(buffer, false);
+        System.out.println(bitmap.setPixels(buffer, false));
 /**/
-        bitmap.writeImage("output");
+
+        Bitmap.Curve curve = new Bitmap.Curve() {
+            @Override
+            public double curve(double input) {
+                if(input > 0.475)
+                    return 1.0;
+                else
+                    return 0.0;
+            }
+        };
+        bitmap0.adjustCurve(curve);
+        bitmap1.adjustCurve(curve);
+        bitmap2.adjustCurve(curve);
+
+        System.out.println(Test.countPixel(bitmap0, -0.1, 0.1));
+        System.out.println(Test.countPixel(bitmap1, -0.1, 0.1));
+        System.out.println(Test.countPixel(bitmap2, -0.1, 0.1));
+
+        bitmap0.writeImage("output0");
+        bitmap1.writeImage("output1");
+        bitmap2.writeImage("output2");
     }
 }
 
